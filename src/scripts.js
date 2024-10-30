28778a32d7a72acbc0761475c7194fcf1b33ebb8
cosmiav <sheptiyanagungrizmawan@students.amikom.ac.id>
2024-10-30 21:50:29 +0700

    Firstt

---
+++ src/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeUpElements = document.querySelectorAll(".fade-up");
+
+  const observerOptions = {
+    threshold: 0, // elemen mulai terlihat pada 10% dari bagiannya
+    rootMargin: "30px 0px",
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // Hanya animasi sekali
+      }
+    });
+  }, observerOptions);
+
+  fadeUpElements.forEach((element) => observer.observe(element));
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Neue Montreal"', "sans-serif"],
+        neuemedium: ['"Neue Montreal medium"', "sans-serif"],
+      },
+
+      animation: {
+        fade: "fadeIn .5s ease-in-out",
+      },
+
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
